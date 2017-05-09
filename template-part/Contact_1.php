05fdfbfae9cf39db3845c1dd779c820b1779417d
Garung <canmotcaiten1993@gmail.com>
2017-05-09 10:41:38 +0700

    add Contact template

---
+++ template-part/Contact_1.php
@@ -0,0 +1,230 @@
+<?php
+/*
+Template Name: Contact
+*/
+get_header();
+?>
+
+<style type="text/css">
+	.contact .title-page {
+	    margin: 0 0 35px;
+	    font-size: 24px;
+	    color: #333333;
+	    font-weight: 700;
+	    border-bottom: 1px solid #cccccc;
+	    padding-bottom: 10px;
+	}
+
+	.contact .contact-wrap h2 {
+	    font-size: 16px;
+	    font-weight: 700;
+	    margin: 0 0 20px;
+	    line-height: 1.3em;
+	    color: #333333;
+	}
+
+	.contact .breadcrumb li a {
+	    color: #666666;
+	    font-size: 13px;
+	}
+
+	.contact .breadcrumb li a.active {
+	    color: #d55449;
+	    text-decoration: none;
+	}
+
+	.contact .Module-141{
+	    text-align: right;
+	    margin-right: 5px;
+	}
+
+</style>
+<div class="container contact">
+	<div class="container">
+			<div class="row Module Module-141">
+				<ol class="breadcrumb" style="background: none;">
+
+		            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="#" class="itemcrumb" itemprop="url"><span itemprop="title">Trang chủ</span></a></li>
+		        
+		            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb"><a href="#" class="itemcrumb active" itemprop="url"><span itemprop="title">Liên Hệ</span></a></li>
+				        
+				    
+				</ol>
+			</div>
+		</div>
+	<div class="row">
+		<div id="ctl00_divCenter" class="middle-fullwidth col-sm-12">
+			<div class="clearfix contact-wrap">
+				<div class="row">
+					<div class="col-sm-12 col-xs-12 Module Module-54">
+						<div class="ModuleContent">
+							<h1 class="title-page">
+							Liên hệ
+							</h1>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4 col-sm-6 mrb20 Module Module-53">
+						<div class="ModuleContent">
+
+						<h2>1. Liên hệ với Phòng Hành chính nhân sự</h2>
+
+						<p>Phụ trách: Lê Văn A</p>
+
+						<p>Email: A@vmms.com.vn</p>
+
+						<p>SĐT: 04 123 4567</p>
+						<br>
+
+						<h2>2. Liên hệ với Phòng Kỹ thuật</h2>
+
+						<p>Phụ trách: Trần Văn B</p>
+
+						<p>Email: B@vmms.com.vn</p>
+
+						<p>SĐT: 04 123 4567</p>
+						<br>
+
+						<h2>3. Liên hệ với Phòng Kinh doanh</h2>
+						<p>Phụ trách: Trần Phương C</p>
+
+						<p>Email: C@vmms.com.vn</p>
+
+						<p>SĐT: 04 123 4567</p>
+						<br>
+
+						</div>
+					</div>
+
+
+					<div class="col-md-4 col-sm-6 mrb20">
+						<div class="clearfix mrb25 Module Module-146">
+							<div class="ModuleContent">
+								<h2>Liên hệ qua email:</h2>
+								Xin vui lòng để lại thông tin, tất cả các thông tin được đánh dấu * là bắt buộc.
+							</div>
+						</div>
+
+						<div class="contact-form Module Module-93">
+							<div class="ModuleContent">
+								<div class="wrap-contact">
+								    <div id="ctl00_mainContent_ctl03_upContact">
+										
+								            <div id="ctl00_mainContent_ctl03_pnlSend" onkeypress="javascript:return WebForm_FireDefaultButton(event, 'ctl00_mainContent_ctl03_btnSend')">
+											
+								                <div class="form-group ct-name">
+								                    <label for="ctl00_mainContent_ctl03_txtName" class="label">
+								                    	Họ tên<span>*</span>
+								                    </label>
+
+								                    <input name="ctl00$mainContent$ctl03$txtName" type="text" maxlength="100" id="ctl00_mainContent_ctl03_txtName" class="form-control" placeholder="Họ tên">
+
+								                    <span id="ctl00_mainContent_ctl03_reqName" title="Vui lòng nhập họ tên." class="fa fa-exclamation-triangle" style="display:none;">
+								                    	
+								                    </span>
+								                </div>
+								                <div class="form-group ct-address">
+								                    <label for="ctl00_mainContent_ctl03_txtAddress" class="label">
+								                    	Địa chỉ
+								                    </label>
+
+								                    <input name="ctl00$mainContent$ctl03$txtAddress" type="text" maxlength="255" id="ctl00_mainContent_ctl03_txtAddress" class="form-control" placeholder="Địa chỉ">
+								                    
+								                </div>
+
+								                <div class="form-group ct-email">
+								                    <label for="ctl00_mainContent_ctl03_txtEmail" class="label">
+								                    	Email<span>*</span>
+								                    </label>
+								                    <input name="ctl00$mainContent$ctl03$txtEmail" type="text" maxlength="255" id="ctl00_mainContent_ctl03_txtEmail" class="form-control" placeholder="Email">
+								                    <span id="ctl00_mainContent_ctl03_reqEmail" title="Vui lòng nhập email." class="fa fa-exclamation-triangle" style="display:none;"></span>
+								                    <span id="ctl00_mainContent_ctl03_regexEmail" title="Email không hợp lệ." class="fa fa-exclamation-triangle" style="display:none;"></span>
+								                </div>
+								                
+								                <div class="form-group ct-phone">
+								                    <label for="ctl00_mainContent_ctl03_txtPhone" class="label">
+								                    	Điện thoại
+								                    </label>
+								                    <input name="ctl00$mainContent$ctl03$txtPhone" type="text" maxlength="255" id="ctl00_mainContent_ctl03_txtPhone" class="form-control" placeholder="Điện thoại">
+								                    
+								                </div>
+								                
+								                <div class="form-group ct-subject">
+								                    <label for="ctl00_mainContent_ctl03_txtSubject" class="label">
+								                    	Tiêu đề
+								                    </label>
+								                    <input name="ctl00$mainContent$ctl03$txtSubject" type="text" maxlength="255" id="ctl00_mainContent_ctl03_txtSubject" class="form-control" placeholder="Tiêu đề">
+								                </div>
+								                <div class="form-group ct-message">
+								                    <label for="ctl00_mainContent_ctl03_txtMessage" class="label">Nội dung<span>*</span></label>
+								                    <textarea name="ctl00$mainContent$ctl03$txtMessage" rows="2" cols="20" id="ctl00_mainContent_ctl03_txtMessage" class="form-control" placeholder="Nội dung"></textarea>
+								                    <span id="ctl00_mainContent_ctl03_reqMessage" title="Vui lòng nhập nội dung liên hệ." class="fa fa-exclamation-triangle" style="display:none;"></span>
+								                </div>
+								                
+								                <div class="clear"></div>
+								                <div class="form-group ct-button">
+								                    <div class="frm-btnwrap">
+								                        <div class="frm-btn">
+								                            <input type="submit" name="ctl00$mainContent$ctl03$btnSend" value="Gửi" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$mainContent$ctl03$btnSend&quot;, &quot;&quot;, true, &quot;Contact93&quot;, &quot;&quot;, false, false))" id="ctl00_mainContent_ctl03_btnSend" class="ct-button btn btn-default">
+								                        </div>
+								                    </div>
+								                </div>
+								            
+										</div>
+								        <div class="clear"></div>
+								            
+								        
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+
+					<div class="col-md-4 col-sm-6 text-center mrb20 Module Module-147">
+						<div class="ModuleContent">
+							<img alt="" width="339" height="456" src="http://fakeimg.pl/339x456">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12 mrb20 Module Module-148">
+						<div class="ModuleContent">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.29003345919!2d105.81594531443692!3d21.02107798600251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab7aba3cad95%3A0x4de4508f754d58cd!2zMjMgTMOhbmcgSOG6oSwgVGjDoG5oIEPDtG5nLCBCYSDEkMOsbmgsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1494299323933" width="100%" height="350" frameborder="0" style="border: 0px;">
+								
+							</iframe>
+						</div>
+					</div>
+
+
+					<div class="col-md-6 col-sm-6 col-xs-12 mrb20 Module Module-149">
+						<div class="ModuleContent">
+							<h2>Công ty Cổ phần Dịch vụ Truyền thông Đa phương tiện Việt Nam (Vietnam Multimedia Services., JSC)</h2>
+
+							<p>Địa chỉ: Phòng 107, Tòa nhà CFM, 23 Láng Hạ, Ba Đình, Hà Nội</p>
+
+							<p>VPGD: Địa chỉ: Phòng 107, Tòa nhà CFM, 23 Láng Hạ, Ba Đình, Hà Nội</p>
+
+							<p>TEL: +84-43-1234567  </p>
+
+							<p>FAX: +84-43-1234567 </p>
+
+							<p>EMAIL: info@vmms.com.vn</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<?php get_footer(); ?>
